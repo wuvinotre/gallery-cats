aa547af6fb84cf993ac66bc8f957e42391140ebb
Ewerton <ewerton.viniciusturco@gmail.com>
2022-08-31 17:52:28 -0300

    Inicio do projeto

--- App.tsx
+++ App.tsx
@@ -1,12 +1,6 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { HomeScreen } from "./src/screens/home";
 
 export default function App() {
-  return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <HomeScreen />;
 }
---
+++ src/screens/home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Text } from "./styled";
+
+export const HomeScreen = () => {
+  return (
+    <Container>
+      <Text>Faz o teste</Text>
+    </Container>
+  );
+};
